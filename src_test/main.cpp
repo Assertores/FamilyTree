9d1937a0b4a480a19d80dd7f941d479f531207c9
Andreas Edmeier <assertores@web.de>
2024-06-09 20:26:40 +0200

    improve test to be used with ci

--- src_test/main.cpp
+++ src_test/main.cpp
@@ -12,42 +12,48 @@
 #include <stdio.h>
 #include <string.h>
 
-#define RUN(aTest)                          \
-	printf("[TEST   ] ===== %s\n", #aTest); \
-	printf("[%s] ///// %s\n", aTest() ? "SUCCESS" : " FAILED", #aTest);
+static bool
+Run(const char* aTestName, bool (*aTest)()) {
+	printf("[TEST   ] ===== %s\n", aTestName);
+	auto result = aTest();
+	printf("[%s] ///// %s\n", result ? "SUCCESS" : " FAILED", aTestName);
+	return result;
+}
+#define RUN(aTest) Run(#aTest, &aTest)
 
 int
 main(int argc, char** argv) {
+	bool result = true;
 	printf("[ SUITE ] JSONPerson\n");
-	RUN(JSONPersonIsZeroIfNoFoldersExist);
-	RUN(JSONPersonIsAmountOfFolders);
-	RUN(CanRetreavePersonIds);
-	RUN(PersonDataCanBeRetreaved);
-	RUN(PlayMusicTriggersCorrectPathOnPlatform);
-	RUN(OpenImageTriggersCorrectPathOnPlatform);
+	result &= RUN(JSONPersonIsZeroIfNoFoldersExist);
+	result &= RUN(JSONPersonIsAmountOfFolders);
+	result &= RUN(CanRetreavePersonIds);
+	result &= RUN(PersonDataCanBeRetreaved);
+	result &= RUN(PlayMusicTriggersCorrectPathOnPlatform);
+	result &= RUN(OpenImageTriggersCorrectPathOnPlatform);
 	printf("\n[ SUITE ] API\n");
-	RUN(CanRetreavePersonThroughAPI);
-	RUN(CanPlayMusicThroughAPI);
-	RUN(CanShowImagesThroughAPI);
-	RUN(CanRetreaveRelationsOfPerson);
+	result &= RUN(CanRetreavePersonThroughAPI);
+	result &= RUN(CanPlayMusicThroughAPI);
+	result &= RUN(CanShowImagesThroughAPI);
+	result &= RUN(CanRetreaveRelationsOfPerson);
 	printf("\n[ SUITE ] CSVRelations\n");
-	RUN(CSVRelationsCanHandleEmptyFile);
-	RUN(CSVRelationsCanComputeCountOfAllUniqueIds);
-	RUN(CSVRelationsCanComputeAllUniqueIds);
-	RUN(CSVRelationsCanComputeRelationsCount);
-	RUN(CSVRelationsCanComputeRelationsOfPerson);
+	result &= RUN(CSVRelationsCanHandleEmptyFile);
+	result &= RUN(CSVRelationsCanComputeCountOfAllUniqueIds);
+	result &= RUN(CSVRelationsCanComputeAllUniqueIds);
+	result &= RUN(CSVRelationsCanComputeRelationsCount);
+	result &= RUN(CSVRelationsCanComputeRelationsOfPerson);
 	printf("\n[ SUITE ] DataProviderComposit\n");
-	RUN(ProviderCompositGetPersonDoesNotLeakInternalId);
-	RUN(ProviderCompositGetRelationsDoesNotLeakInternalId);
+	result &= RUN(ProviderCompositGetPersonDoesNotLeakInternalId);
+	result &= RUN(ProviderCompositGetRelationsDoesNotLeakInternalId);
 	printf("\n[ SUITE ] Algorythems\n");
-	RUN(ChildRelationIsCorrectlyUnderstood);
-	RUN(ParentRelationIsCorrectlyUnderstood);
-	RUN(GrandParentAndChildRelationIsCorrectlyUnderstood);
+	result &= RUN(ChildRelationIsCorrectlyUnderstood);
+	result &= RUN(ParentRelationIsCorrectlyUnderstood);
+	result &= RUN(GrandParentAndChildRelationIsCorrectlyUnderstood);
 
 #ifndef NDEBUG
 	if (_CrtDumpMemoryLeaks()) {
 		printf("\n!!!!! >> A memory leak was detected << !!!!!\n");
 	}
 #endif
-	return 0;
+	return result ? 0 : 1;
 }
