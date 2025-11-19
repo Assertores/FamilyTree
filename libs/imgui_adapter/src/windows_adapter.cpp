9e3d3ad1fdc3c2b2e82ff6fe8223027c57d76e29
Andreas Edmeier <assertores@web.de>
2025-11-19 23:53:56 +0100

    fix naming convention and codestyle

--- libs/imgui_adapter/src/windows_adapter.cpp
+++ libs/imgui_adapter/src/windows_adapter.cpp
@@ -9,8 +9,6 @@
 // This is provided for completeness, however it is strongly recommended you use OpenGL with SDL or
 // GLFW.
 
-// NOLINTBEGIN
-
 #include "windows_adapter.hpp"
 
 #include "imgui.h"
@@ -29,56 +27,63 @@ ImGuiAdapter::CreateWindows() { // NOLINT
 }
 
 // Data stored per platform window
-struct WGL_WindowData {
+struct WglWindowData {
 	HDC hDC;
 };
 
 // Data
-static HGLRC g_hRC;
-static WGL_WindowData g_MainWindow;
-static int g_Width;
-static int g_Height;
+static HGLRC theHRC;
+static WglWindowData theMainWindow;
+static int theWidth;
+static int theHeight;
 
 // Forward declarations of helper functions
-bool CreateDeviceWGL(HWND hWnd, WGL_WindowData* data);
-void CleanupDeviceWGL(HWND hWnd, WGL_WindowData* data);
+bool CreateDeviceWGL(HWND aHwnd, WglWindowData* aData);
+void CleanupDeviceWGL(HWND aHwnd, WglWindowData* aData);
 void ResetDeviceWGL();
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT WINAPI WndProc(HWND aHwnd, UINT aMsg, WPARAM aWParam, LPARAM aLParam);
 
 // Support function for multi-viewports
 // Unlike most other backend combination, we need specific hooks to combine Win32+OpenGL.
 // We could in theory decide to support Win32-specific code in OpenGL backend via e.g. an
 // hypothetical ImGui_ImplOpenGL3_InitForRawWin32().
 static void
-Hook_Renderer_CreateWindow(ImGuiViewport* viewport) {
-	assert(viewport->RendererUserData == NULL);
+HookRendererCreateWindow(ImGuiViewport* aViewport) {
+	assert(aViewport->RendererUserData == nullptr);
 
-	WGL_WindowData* data = IM_NEW(WGL_WindowData);
-	CreateDeviceWGL((HWND)viewport->PlatformHandle, data);
-	viewport->RendererUserData = data;
+	auto* data = IM_NEW(WglWindowData);
+	// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast)
+	CreateDeviceWGL((HWND)aViewport->PlatformHandle, data);
+	aViewport->RendererUserData = data;
 }
 
 static void
-Hook_Renderer_DestroyWindow(ImGuiViewport* viewport) {
-	if (viewport->RendererUserData != NULL) {
-		WGL_WindowData* data = (WGL_WindowData*)viewport->RendererUserData;
-		CleanupDeviceWGL((HWND)viewport->PlatformHandle, data);
+HookRendererDestroyWindow(ImGuiViewport* aViewport) {
+	if (aViewport->RendererUserData != nullptr) {
+		// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast)
+		auto* data = (WglWindowData*)aViewport->RendererUserData;
+		// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast)
+		CleanupDeviceWGL((HWND)aViewport->PlatformHandle, data);
 		IM_DELETE(data);
-		viewport->RendererUserData = NULL;
+		aViewport->RendererUserData = nullptr;
 	}
 }
 
 static void
-Hook_Platform_RenderWindow(ImGuiViewport* viewport, void*) {
+HookPlatformRenderWindow(ImGuiViewport* aViewport, void* /*unused*/) {
 	// Activate the platform window DC in the OpenGL rendering context
-	if (WGL_WindowData* data = (WGL_WindowData*)viewport->RendererUserData)
-		wglMakeCurrent(data->hDC, g_hRC);
+	// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast)
+	if (auto* data = (WglWindowData*)aViewport->RendererUserData) {
+		wglMakeCurrent(data->hDC, theHRC);
+	}
 }
 
 static void
-Hook_Renderer_SwapBuffers(ImGuiViewport* viewport, void*) {
-	if (WGL_WindowData* data = (WGL_WindowData*)viewport->RendererUserData)
+HookRendererSwapBuffers(ImGuiViewport* aViewport, void* /*unused*/) {
+	// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast)
+	if (auto* data = (WglWindowData*)aViewport->RendererUserData) {
 		::SwapBuffers(data->hDC);
+	}
 }
 
 void
@@ -113,13 +118,13 @@ WindowsAdapter::Init(int aWidth, int aHight, std::string aName, ImVec4 aClearCol
 		nullptr);
 
 	// Initialize OpenGL
-	if (!CreateDeviceWGL(myHwnd, &g_MainWindow)) {
-		CleanupDeviceWGL(myHwnd, &g_MainWindow);
+	if (!CreateDeviceWGL(myHwnd, &theMainWindow)) {
+		CleanupDeviceWGL(myHwnd, &theMainWindow);
 		::DestroyWindow(myHwnd);
 		::UnregisterClassW(myWc.lpszClassName, myWc.hInstance);
 		return;
 	}
-	wglMakeCurrent(g_MainWindow.hDC, g_hRC);
+	wglMakeCurrent(theMainWindow.hDC, theHRC);
 
 	// Show the window
 	::ShowWindow(myHwnd, SW_SHOWDEFAULT);
@@ -128,11 +133,15 @@ WindowsAdapter::Init(int aWidth, int aHight, std::string aName, ImVec4 aClearCol
 	// Setup Dear ImGui context
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	(void)io;
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
-	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable; // Enable Multi-Viewport / Platform Windows
+	ImGuiIO& locIo = ImGui::GetIO();
+	(void)locIo;
+	// NOLINTBEGIN
+	locIo.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+	locIo.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;	 // Enable Gamepad Controls
+	locIo.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;	 // Enable Multi-Viewport / Platform
+															 // Windows
+
+	// NOLINTEND
 
 	// Setup Dear ImGui style
 	ImGui::StyleColorsDark();
@@ -141,10 +150,8 @@ WindowsAdapter::Init(int aWidth, int aHight, std::string aName, ImVec4 aClearCol
 	// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look
 	// identical to regular ones.
 	ImGuiStyle& style = ImGui::GetStyle();
-	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-		style.WindowRounding = 0.0f;
-		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-	}
+	style.WindowRounding = 0.0F;
+	style.Colors[ImGuiCol_WindowBg].w = 1.0F;
 
 	// Setup Platform/Renderer backends
 	ImGui_ImplWin32_InitForOpenGL(myHwnd);
@@ -152,17 +159,15 @@ WindowsAdapter::Init(int aWidth, int aHight, std::string aName, ImVec4 aClearCol
 
 	// Win32+GL needs specific hooks for viewport, as there are specific things needed to tie Win32
 	// and GL api.
-	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-		ImGuiPlatformIO& platform_io = ImGui::GetPlatformIO();
-		IM_ASSERT(platform_io.Renderer_CreateWindow == NULL);
-		IM_ASSERT(platform_io.Renderer_DestroyWindow == NULL);
-		IM_ASSERT(platform_io.Renderer_SwapBuffers == NULL);
-		IM_ASSERT(platform_io.Platform_RenderWindow == NULL);
-		platform_io.Renderer_CreateWindow = Hook_Renderer_CreateWindow;
-		platform_io.Renderer_DestroyWindow = Hook_Renderer_DestroyWindow;
-		platform_io.Renderer_SwapBuffers = Hook_Renderer_SwapBuffers;
-		platform_io.Platform_RenderWindow = Hook_Platform_RenderWindow;
-	}
+	ImGuiPlatformIO& platformIo = ImGui::GetPlatformIO();
+	IM_ASSERT(platformIo.Renderer_CreateWindow == nullptr);
+	IM_ASSERT(platformIo.Renderer_DestroyWindow == nullptr);
+	IM_ASSERT(platformIo.Renderer_SwapBuffers == nullptr);
+	IM_ASSERT(platformIo.Platform_RenderWindow == nullptr);
+	platformIo.Renderer_CreateWindow = HookRendererCreateWindow;
+	platformIo.Renderer_DestroyWindow = HookRendererDestroyWindow;
+	platformIo.Renderer_SwapBuffers = HookRendererSwapBuffers;
+	platformIo.Platform_RenderWindow = HookPlatformRenderWindow;
 
 	// Load Fonts
 	// - If no fonts are loaded, dear imgui will use the default font. You can also load multiple
@@ -199,10 +204,12 @@ WindowsAdapter::Update() {
 	// Poll and handle messages (inputs, window resize, etc.)
 	// See the WndProc() function below for our to dispatch events to the Win32 backend.
 	MSG msg;
-	while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE)) {
+	while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE) != 0) {
 		::TranslateMessage(&msg);
 		::DispatchMessage(&msg);
-		if (msg.message == WM_QUIT) return false;
+		if (msg.message == WM_QUIT) {
+			return false;
+		}
 	}
 
 	// Start the Dear ImGui frame
@@ -215,28 +222,29 @@ WindowsAdapter::Update() {
 
 void
 WindowsAdapter::Show() {
-	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	ImVec4 constexpr clearColor = ImVec4(0.45F, 0.55F, 0.60F, 1.00F);
 	// Rendering
 	ImGui::Render();
-	glViewport(0, 0, g_Width, g_Height);
-	glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+	glViewport(0, 0, theWidth, theHeight);
+	glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
 	glClear(GL_COLOR_BUFFER_BIT);
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 	// Update and Render additional Platform Windows
-	ImGuiIO& io = ImGui::GetIO();
-	(void)io;
-	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+	ImGuiIO& locIo = ImGui::GetIO();
+	(void)locIo;
+	// NOLINTNEXTLINE(hicpp-signed-bitwise)
+	if ((locIo.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) != 0) {
 		ImGui::UpdatePlatformWindows();
 		ImGui::RenderPlatformWindowsDefault();
 
 		// Restore the OpenGL rendering context to the main window DC, since platform windows might
 		// have changed it.
-		wglMakeCurrent(g_MainWindow.hDC, g_hRC);
+		wglMakeCurrent(theMainWindow.hDC, theHRC);
 	}
 
 	// Present
-	::SwapBuffers(g_MainWindow.hDC);
+	::SwapBuffers(theMainWindow.hDC);
 }
 
 void
@@ -245,42 +253,50 @@ WindowsAdapter::Shutdown() {
 	ImGui_ImplWin32_Shutdown();
 	ImGui::DestroyContext();
 
-	CleanupDeviceWGL(myHwnd, &g_MainWindow);
-	wglDeleteContext(g_hRC);
+	CleanupDeviceWGL(myHwnd, &theMainWindow);
+	wglDeleteContext(theHRC);
 	::DestroyWindow(myHwnd);
 	::UnregisterClassW(myWc.lpszClassName, myWc.hInstance);
 }
 
 // Helper functions
 bool
-CreateDeviceWGL(HWND hWnd, WGL_WindowData* data) {
-	HDC hDc = ::GetDC(hWnd);
+CreateDeviceWGL(HWND aHwnd, WglWindowData* aData) {
+	HDC hDc = ::GetDC(aHwnd);
 	PIXELFORMATDESCRIPTOR pfd = {0};
 	pfd.nSize = sizeof(pfd);
 	pfd.nVersion = 1;
 	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
 	pfd.iPixelType = PFD_TYPE_RGBA;
-	pfd.cColorBits = 32;
+	pfd.cColorBits = 32; // NOLINT
 
-	const int pf = ::ChoosePixelFormat(hDc, &pfd);
-	if (pf == 0) return false;
-	if (::SetPixelFormat(hDc, pf, &pfd) == FALSE) return false;
-	::ReleaseDC(hWnd, hDc);
+	const int locpf = ::ChoosePixelFormat(hDc, &pfd);
+	if (locpf == 0) {
+		return false;
+	}
+	if (::SetPixelFormat(hDc, locpf, &pfd) == FALSE) {
+		return false;
+	}
+	::ReleaseDC(aHwnd, hDc);
 
-	data->hDC = ::GetDC(hWnd);
-	if (!g_hRC) g_hRC = wglCreateContext(data->hDC);
+	aData->hDC = ::GetDC(aHwnd);
+	if (theHRC == nullptr) {
+		theHRC = wglCreateContext(aData->hDC);
+	}
 	return true;
 }
 
 void
-CleanupDeviceWGL(HWND hWnd, WGL_WindowData* data) {
+CleanupDeviceWGL(HWND aHwnd, WglWindowData* aData) {
 	wglMakeCurrent(nullptr, nullptr);
-	::ReleaseDC(hWnd, data->hDC);
+	::ReleaseDC(aHwnd, aData->hDC);
 }
 
+// NOLINTBEGIN
 // Forward declare message handler from imgui_impl_win32.cpp
 extern IMGUI_IMPL_API LRESULT
 ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+// NOLINTEND
 
 // Win32 message handler
 // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to
@@ -291,24 +307,29 @@ ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam
 // application, or clear/overwrite your copy of the keyboard data. Generally you may always pass all
 // inputs to dear imgui, and hide them from your application based on those two flags.
 LRESULT WINAPI
-WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam)) return true;
+WndProc(HWND aHwnd, UINT aMsg, WPARAM aWParam, LPARAM aLParam) {
+	if (ImGui_ImplWin32_WndProcHandler(aHwnd, aMsg, aWParam, aLParam) != 0) {
+		return 1;
+	}
 
-	switch (msg) {
+	switch (aMsg) {
 	case WM_SIZE:
-		if (wParam != SIZE_MINIMIZED) {
-			g_Width = LOWORD(lParam);
-			g_Height = HIWORD(lParam);
+		if (aWParam != SIZE_MINIMIZED) {
+			theWidth = LOWORD(aLParam);
+			theHeight = HIWORD(aLParam);
 		}
 		return 0;
 	case WM_SYSCOMMAND:
-		if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+		// NOLINTNEXTLINE
+		if ((aWParam & 0xfff0) == SC_KEYMENU) { // Disable ALT application menu
 			return 0;
+		}
 		break;
 	case WM_DESTROY:
 		::PostQuitMessage(0);
 		return 0;
+	default:
+		break;
 	}
-	return ::DefWindowProcW(hWnd, msg, wParam, lParam);
+	return ::DefWindowProcW(aHwnd, aMsg, aWParam, aLParam);
 }
-// NOLINTEND
