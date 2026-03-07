b0afc1d995b83e94347ccc52467fc779d101f975
Andreas Edmeier <assertores@web.de>
2026-03-07 14:48:16 +0100

    impliment linux adapter

--- libs/imgui_adapter/src/linux_adapter.hpp
+++ libs/imgui_adapter/src/linux_adapter.hpp
@@ -1,1 +1,17 @@
 #include "imgui_adapter/imgui_adapter.hpp"
+
+#include <SDL2/SDL.h>
+
+// NOLINTBEGIN
+class LinuxAdapter : public ImGuiAdapter {
+public:
+	void Init(int aWidth, int aHight, std::string aName, ImVec4 aClearColor) override;
+	bool Update() override;
+	void Show() override;
+	void Shutdown() override;
+
+private:
+	SDL_Window* window;
+	ImVec4 clear_color;
+};
+// NOLINTEND
