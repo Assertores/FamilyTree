3e56c09d94f7e388040b95d763f92974f551a2f7
Andreas Edmeier <assertores@web.de>
2026-08-25 15:55:30 +0200

    improve Testing framework

--- lib_family_tree/tests/src/check.hpp
+++ lib_family_tree/tests/src/check.hpp
@@ -1,10 +1,37 @@
 #pragma once
 
-#include <algorithm>
-#include <array>
 #include <iostream>
+#include <optional>
 #include <sstream>
 #include <string_view>
+#include <variant>
+#include <vector>
+
+#if _WIN32
+#include <csignal>
+#include <stdexcept>
+#include <string>
+
+namespace intern {
+void
+SignalHandler(int aSignal) {
+	throw std::runtime_error("Signal: " + std::to_string(aSignal));
+}
+} // namespace intern
+#else
+#endif
+
+#ifndef NDEBUG
+#if _WIN32
+#include <crtdbg.h>
+#define LEAK_CHECK _CrtDumpMemoryLeaks() != 0
+#else
+#include <sanitizer/lsan_interface.h>
+#define LEAK_CHECK __lsan_do_recoverable_leak_check() != 0
+#endif
+#else
+#define LEAK_CHECK false
+#endif
 
 namespace intern {
 template <class Lhs, class Rhs>
@@ -16,13 +43,70 @@ Equal(const Lhs& aLhs, const Rhs& aRhs) {
 template <class Lhs = char*, class Rhs = char*>
 bool
 Equal(const char* aLhs, const char* aRhs) {
-	if(aLhs == aRhs){
+	if (aLhs == aRhs) {
 		return true;
 	}
-	if(aLhs == nullptr){
+	if (aLhs == nullptr) {
 		return false;
 	}
 	return std::string_view(aLhs) == aRhs;
+}
+
+template <class... T, class Lhs = std::variant<T...>, class Rhs>
+bool
+Equal(const std::variant<T...>& aLhs, const Rhs& aRhs) {
+	if (!std::holds_alternative<Rhs>(aLhs)) {
+		return false;
+	}
+	return Equal(std::get<Rhs>(aLhs), aRhs);
+}
+
+template <class T, class Lhs = std::optional<T>, class Rhs>
+bool
+Equal(const std::optional<T>& aLhs, const Rhs& aRhs) {
+	if (!aLhs) {
+		return false;
+	}
+	if (std::is_same_v<Rhs, std::nullopt_t>) {
+		return false;
+	}
+	return Equal(aLhs.value(), aRhs);
+}
+
+template <class T>
+static std::string
+Print(const T& aValue) {
+	std::stringstream stream;
+	stream << aValue;
+	return stream.str();
+}
+
+template <typename T>
+static std::string
+Print(const std::vector<T>& aVector) {
+	std::stringstream stream;
+	for (const T& element : aVector) {
+		stream << Print(element) << ' ';
+	}
+	return stream.str();
+}
+
+template <typename... T>
+static std::string
+Print(const std::variant<T...>& aVariant) {
+	return std::visit([&](const auto& aValue) { return Print(aValue); }, aVariant);
+}
+
+template <class T = std::nullopt_t>
+static std::string
+Print(const std::nullopt_t& /*unused*/) {
+	return "NullOpt";
+}
+
+template <typename T>
+static std::string
+Print(const std::optional<T>& aOptional) {
+	return aOptional ? Print(aOptional.value()) : Print(std::nullopt);
 }
 
 template <class T, class... E>
@@ -33,11 +117,11 @@ Check(
 	std::string_view aVariableName,
 	const T& aVariable,
 	E... aExpectations) {
-	bool result = (Equal(aVariable, aExpectations) || ...);
+	const bool result = (Equal(aVariable, aExpectations) || ...);
 	if (!result) {
 		std::cout << aFile << " (" << aLine << ")\n" << aVariableName << " expected to be: ";
-		((std::cout << aExpectations << ", "), ...);
-		std::cout << "but actualy: " << aVariable << '\n';
+		((std::cout << Print(aExpectations) << ", "), ...);
+		std::cout << "but actualy: " << Print(aVariable) << '\n';
 	}
 	return result;
 }
@@ -50,11 +134,11 @@ CheckExclude(
 	std::string_view aVariableName,
 	const T& aVariable,
 	E... aExpectations) {
-	bool result = !(Equal(aVariable, aExpectations) || ...);
+	const bool result = !(Equal(aVariable, aExpectations) || ...);
 	if (!result) {
 		std::cout << aFile << " (" << aLine << ")\n" << aVariableName << " expected to not be: ";
-		((std::cout << aExpectations << ", "), ...);
-		std::cout << "but actualy: " << aVariable << '\n';
+		((std::cout << Print(aExpectations) << ", "), ...);
+		std::cout << "but actualy: " << Print(aVariable) << '\n';
 	}
 	return result;
 }
@@ -73,6 +157,57 @@ Run(const char* aTestName, bool (*aTest)()) {
 	std::cout << "[" << (result ? "SUCCESS" : " FAILED") << "]\n";
 	return result;
 }
+
+class ResultTracker {
+public:
+	explicit ResultTracker(bool aIsTestSuit)
+		: myIsTestSuit(aIsTestSuit) {}
+	ResultTracker(const ResultTracker&) = delete;
+	ResultTracker& operator=(const ResultTracker&) = delete;
+
+	ResultTracker(ResultTracker&&) = delete;
+	ResultTracker& operator=(ResultTracker&&) = delete;
+
+	~ResultTracker() {
+		if (!myIsTestSuit && LEAK_CHECK) {
+			std::cout << "\n!!!!! >> A memory leak was detected << !!!!!\n";
+		}
+
+		std::cout << (myIsTestSuit ? "[ SUITE ] " : "\n")
+				  << (myResult ? "All Succeeded" : "Failure detected") << '\n';
+		if (!myIsTestSuit) {
+			std::cout << "===== DONE =====\n";
+		}
+	}
+
+	ResultTracker& operator&=(bool aNewValue) {
+		myResult &= aNewValue;
+		return *this;
+	}
+
+	// NOLINTNEXTLINE
+	operator int() const { return myResult ? 0 : 1; }
+	// NOLINTNEXTLINE
+	operator bool() const { return myResult; }
+
+private:
+	bool myResult = true;
+	bool myIsTestSuit = false;
+};
+
+static ResultTracker
+Setup() {
+#if _WIN32
+	(void)signal(SIGSEGV, SignalHandler);
+#endif
+	return ResultTracker(false);
+}
+
+static ResultTracker
+SetupSuit(std::string_view aName) {
+	std::cout << "\n[ SUITE ] " << aName << '\n';
+	return ResultTracker(true);
+}
 } // namespace intern
 
 #define CHECK(aValue, ...) \
@@ -80,3 +215,6 @@ Run(const char* aTestName, bool (*aTest)()) {
 #define CHECK_EXCLUDE(aValue, ...) \
 	if (!intern::CheckExclude(__FILE__, __LINE__, #aValue, aValue, __VA_ARGS__)) return false
 #define RUN(aTest) intern::Run(#aTest, &(aTest))
+
+#define SETUP() intern::Setup()
+#define SUIT(aName) intern::SetupSuit(aName)
